a6f189f1e938add5a27031638ed64a01d53612d8
josh619 <vjoshuadan@gmail.com>
2024-03-06 18:26:28 +0530

    pdf setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import PdfViewer from "./pages/pdfViewer/pdfViewer"
+function App() {
+  return (
+    <div className="App">
+    <PdfViewer />
+    </div>
+  );
+}
+
+export default App;
